cc291d2b02db09c4782a4b1ce0a85e28a352bab4
Urszula Patyra <upatyra@wp.pl>
2022-02-11 20:09:04 +0100

    Add project files

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -51,8 +51,22 @@
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
+class Product{
+  constructor(id, data){
+    const thisProduct = this;
 
+    console.log('new Product:', thisProduct);
+  }
+}
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,8 +74,16 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
+  initData: function(){
+    const thisApp = this;
+
+    thisApp.data = dataSource;
+  }
 
   app.init();
 }
