d48ddf06b370be6f54f91400e8be19d502068fbc
Urszula Patyra <upatyra@wp.pl>
2022-03-09 23:46:28 +0100

    Start widget class

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -86,6 +86,7 @@
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
     }
     initAccordion(){
       const thisProduct = this;
@@ -130,6 +131,8 @@
         thisProduct.processOrder();
       });
     }
+    initAmountWidget();
+
     processOrder() {
       const thisProduct = this;
     
@@ -175,6 +178,49 @@
     
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
+    }
+    initAmountWidget(){
+      const thisProduct = this;
+
+      thisProduct.amountWidget = newAmountWidget(thisProduct.amountWidgetElem);
+    }
+  }
+  class AmountWidget{
+    constructor(element){
+      const thisWidget = this;
+
+      console.log('AmountWidget:', thisWidget);
+      console.log('constructor arguments:', element);
+      thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
+    }
+    getElements(element){
+      const thisWidget = this;
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+    setValue(value){
+      const thisWidget = this;
+
+      const newValue = parseInt(value);
+      /* TODO: Add validation */
+      if(thisWidget.value !== newValue  && !isNaN(newValue)) {
+        thisWidget.value = newValue;
+      }
+
+      thisWidget.input.value = thisWidget.value;
+
+    }
+    initActions(){
+      thisWidget.input.addEventListener('change', setValue(value));
+      thisWidget.linkDecrease.addEventListener('click', function(event){
+        event.preventDefault();
+        const decreasedValue -= thisWidget.value;
+        const increasedValue += thisWidget.value;
+
+      })
     }
   }
   const app = {
