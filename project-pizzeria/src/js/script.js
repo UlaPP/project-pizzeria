2350967870b4d3ebb63f9bfe0eb78127719243df
Urszula Patyra <upatyra@wp.pl>
2022-04-11 22:56:03 +0200

    Add cart class

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -277,6 +277,32 @@
       thisWidget.element.dispatchEvent(event);
     }
   }
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('new Cart', thisCart);
+    }
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+    initActions(){
+      const thisCart = this;
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
   const app = {
     initMenu: function(){
       const thisApp = this;
@@ -285,7 +311,6 @@
         new Product(productData, thisApp.data.products[productData]);
       }
     },
-
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -296,11 +321,18 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
     initData: function(){
       const thisApp = this;
   
       thisApp.data = dataSource;
+    },
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.carts = new Cart(cartElem);
     }
   };
 
